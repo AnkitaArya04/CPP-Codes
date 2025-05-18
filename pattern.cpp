0e2de8c94b2f4cfa4745407a83fe539c76c363cb
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-18 23:14:01 +0530

    pattern 1

---
+++ pattern.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int n;
+    // cin >> n;
+    // int i,j;
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         cout << "*";
+    //     }
+    //     cout << "" << endl;
+    // }
+
+
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        /* code */
+        int j=1;
+        while (j<=n)
+        {
+            /* code */
+            cout << "*";
+            j=j+1;
+
+      }
+      cout << endl;
+      i=i+1;
+        
+    }
+    
+}
