77ac8465736ce7e0ec5a63a33ce423f984b49662
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-18 14:22:39 +0530

    basic operators

--- hello.cpp
+++ hello.cpp
@@ -1,17 +1,49 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a=123;
-    cout << a << endl;
-    char b='c';
-    cout << b << endl;
-    bool bl = true;
-    cout << bl << endl;
-    float f=1.2;
-    cout << f << endl;
-    double d=1.23;
-    cout <<  d << endl;
+    // int a=123;
+    // cout << a << endl;
+    // char b='c';
+    // cout << b << endl;
+    // bool bl = true;
+    // cout << bl << endl;
+    // float f=1.2;
+    // cout << f << endl;
+    // double d=1.23;
+    // cout <<  d << endl;
 
-    int size=sizeof(a);
-    cout << size << endl;
+    // int size=sizeof(a);
+    // cout << size << endl;
+    // int a='a';
+    // cout << a << endl;
+
+    // char ch=98;
+    // cout << ch << endl;
+
+    // int c=-27;
+    // cout << c << endl;
+
+    // unsigned int a=-23;
+    // cout << a << endl;
+
+    // float a=2.0/5;
+    // cout << a << endl;
+
+    // int a=2;
+    // int b=3;
+    // bool first=(a==b);
+    // cout << first << endl;
+
+    // bool second = (a>b);
+    // cout << second << endl;
+
+    // bool third = (a<b);
+    // cout << third << endl;
+
+    // bool fourth =(a>=b);
+    // cout << fourth << endl;
+
+    int a=23;
+    cout << !a << endl;
+
 }
