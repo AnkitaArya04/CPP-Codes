b6cb1d7a10e672a4fc272a35d4196125602191a6
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-18 16:08:57 +0530

    conditionals in CPP

---
+++ conditionals.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a;
+    // cin >> a;
+    // cout << " the value of n is :" << n << endl;
+    // if(a>0){
+    //     cout << "a is positive" << endl;
+    // }else{
+    //     cout << "a is negative" << endl;
+    // }
+    int a,b;
+    cout << "enter the value of a" << endl;
+    cin >> a;
+    cout << "enter the value of b" <<endl;
+    cin >> b;
+    if(a>b){
+        cout << "A is greater" << endl;
+    }else{
+        cout << "B is greater" << endl;
+    }
+
+
+}
