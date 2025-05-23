a81c10b269b368ae0fd67a664075e2eda02ff74d
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-23 23:00:24 +0530

    bitwise operator

--- bitwise.cpp
+++ bitwise.cpp
@@ -7,4 +7,15 @@ int main(){
     cout << "a|b " << (a|b) << endl;
     cout << "~a " << ~a << endl;
     cout << "a^b " << (a^b) << endl;
+
+    cout << (17>>1) << endl;
+    cout << (17>>2) << endl;
+    cout<<(19<<1)<< endl;
+    cout << (21<<2) << endl;
+
+    int i=7;
+    cout << (++i) << endl;
+    cout << (i++) << endl;
+    cout << (--i) << endl;
+    cout << (i++) << endl;
 }
