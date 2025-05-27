c239fd4516dbb82509425a5d8efbd497e9257ce6
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-27 21:19:22 +0530

    Array

---
+++ array1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void printArray(){
+
+}
+int main(){
+    int number[15];
+    // cout << number[1] << endl;
+    // cout << number[201] << endl;
+
+    int second[3]={5,7,11};
+    cout << second[1];
+}
