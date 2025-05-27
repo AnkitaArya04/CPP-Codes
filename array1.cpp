aae35a83c94c80719d88bb6b971f09d6791621bc
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-27 21:22:22 +0530

    Array

--- array1.cpp
+++ array1.cpp
@@ -10,5 +10,13 @@ int main(){
     // cout << number[201] << endl;
 
     int second[3]={5,7,11};
-    cout << second[1];
+    // cout << second[1];
+
+    int third[15]={2,7};
+    int n=15;
+    cout << "printing the array" << endl;
+    for(int i=0;i<n;i++){
+        cout << third[i] << endl;
+    }
+    // cout << third[];
 }
