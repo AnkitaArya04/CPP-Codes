a6bbfcc1de2b111c598fcf84f2933c06f68c944a
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-28 19:43:21 +0530

    printing the Array

--- array1.cpp
+++ array1.cpp
@@ -4,26 +4,35 @@ using namespace std;
 void printArray(int arr[],int size){
     cout << "printing the array" << endl;
     for(int i=0;i<size;i++){
-        cout << arr[i] << " ";
+        // cout << arr[i] << " ";
     }
-    cout << "printing done" << endl;
+    // cout << "printing done" << endl;
 
 }
 int main(){
-    int number[15];
+    // int number[15];
     // cout << number[1] << endl;
     // cout << number[201] << endl;
 
-    int second[3]={5,7,11};
+    // int second[3]={5,7,11};
     // cout << second[1];
 
-    int third[15]={2,7};
-    int n=15;
-    printArray(third,n);
+    // int third[15]={2,7};
+    // int n=15;
+    // printArray(third,n);
 
     // int fourth[10]={0};
     // int n=10;
     // for(int i=0;i<n;i++){
     //     cout << fourth[i] << " ";
     // }
+
+    // cout << ch[1] ;
+    char ch[5]={'a','b','c','e','d'};
+    cout << ch[1];
+
+    cout << "printing the array:" << endl;
+    for(int i=0;i<5;i++){
+        cout << ch[i] << " ";
+    }
 }
