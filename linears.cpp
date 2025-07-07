196cc6dba98b8cec7ea280ebb6cfa0b700814556
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-07 13:27:21 +0530

    linear

---
+++ linears.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int linear_search(int arr[],int size,int target){
+    for(int i=0;i<size;i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    int arr[]={4,2,7,8,1,2,5};
+    int size=7;
+    int target=7;
+    cout << linear_search(arr,size,target) << endl;
+}
